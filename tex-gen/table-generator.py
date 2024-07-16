3495c62e9e8fbb6aedec53b59977dbb22e7c6305
Andreas Herten <a.herten@fz-juelich.de>
2024-07-16 08:48:56 +0200

    First implementation of generator
    See `--help` for options (which are mostly print and write currently). It expects the benchmarks.yaml in the same directory.
    
    The script goes through the YAML and first prints the suite, then goes through all the benchmarks of the suites, and finally prints the standalone benchmarks.
    
    Most of the setup logic is in Python, and not in Jinja, since I want to reuse the same template for suites and benchmarks. I think it's a nice way.
    
    There's still things to be done (beyond bugfixes), most prominently the vertical line. But this is mostly esthetical, and I will add it later.

--- tex-gen/table-generator.py
+++ tex-gen/table-generator.py
@@ -8,7 +8,11 @@ from jinja2 import Environment, BaseLoader
 tags_cats_to_tex_commands = {
     'programming-model': 'btProgMod',
     'programming-language': 'btProgLang',
-    'benchmark-scale': 'btScale'
+    'benchmark-scale': 'btScale',
+    'memory-access-characteristics': 'btMemAcc',
+    'method-type': 'btMethod',
+    'communication': 'btComm',
+    'application-domain': 'btAppDomain'
 }
 sort_order_tags = {cat: (i, cat) for i, cat in enumerate(["programming-language", "programming-model"])}
 def customSortTags(tags):
@@ -20,6 +24,16 @@ def parseTags(rawTags):
         cat = tags_cats_to_tex_commands[rawcat] if rawcat in tags_cats_to_tex_commands else 'NA'
         _tags.append((cat, name))
     return _tags
+tpl_suite_header = r"""
+    ((* if suite -*))\multicolumn{2}{l}{((( name )))} & 
+    ((*- else -*))
+    & ((( name ))) &
+    ((*- endif *))
+    ((* for tag in tags *)) \((( tag[0] ))){((( tag[1] )))} ((*- endfor *)) &
+    ((* if license -*)) ((( license ))) ((* endif -*)) & 
+    ((* if url['long'] -*)) \href{((( url['long'] )))}{((( url['short'] )))}} ((* endif -*)) ((* if citekey -*)) \cite{((( citekey )))} ((* endif -*)) & 
+    ((* if notes -*)) ((( notes ))) ((* endif -*))\\
+"""
 def main(args):
 
     with open('benchmarks.yaml', 'r') as f:
@@ -39,97 +53,34 @@ def main(args):
         loader=BaseLoader
     )
 
-    # Artifact association
-
-    bench_rel_tpl = r"""
-    % this file is auto-generated, do not change it manually!
-    $A_{((( number )))}$ is the benchmark setup for $C_{1,((( number )))}$ ( (((- name -))) )."""
-
-    bench_exp_tpl = r"""
-    % this file is auto-generated, do not change it manually!
-    We expect a seamless setup using the provided repository, following the instructions in \texttt{DESCRIPTION.md}. ((* if base -*)) Given a similar execution environment, scaling results similar to the ones provided in Figure 2 ((* if weak -*)) and Figure 3 ((* endif -*)) of the manuscript. ((* endif -*))"""
-
-    bench_artsw_tpl = r"""
-    % this file is auto-generated, do not change it manually!
-    $A_{((( number )))}$ ( (((- name -))) ) uses EasyBuild modules of the following software components (including version numbers): ((( modules|join(', ') )))"""
-
-    bench_artdata_tpl = r"""
-    % this file is auto-generated, do not change it manually!
-    The benchmark utilizes no input dataset; all data is generated on-the-fly as part of the benchmark."""
-
-    bench_artdeploy_tpl = r"""
-    % this file is auto-generated, do not change it manually!
-    ((* if name in compile_strings *))Compilation is integrated into the JUBE script and described in detail in \texttt{DESCRIPTION.md} in the benchmark's repository. Relevant for the compilation of ((( name ))) is the following snippet: 
-    \begin{minted}{bash}
-    ((( compile_strings[name] -)))
-    \end{minted}
-    ((* else *))
-    The benchmark does not need to be compiled.
-    ((* endif -*))
-    """
-
-    bench_artcomp_tpl = r"""
-    % this file is auto-generated, do not change it manually!
-    $A_{((( number )))}$ ( (((- name -))) ) includes the file ((*- if jubefiles | length > 1 -*))s((*- endif *)) ((*- for jubefile in jubefiles *)) \Verb|((( jubefile )))| ((*- if (loop.length > 2) and (loop.index != loop.length) -*)), ((*- endif -*)) ((*- if loop.index == loop.length - 1 *)) and ((*- endif -*)) ((* endfor *)), which contain ((*- if jubefiles | length == 1 -*))s((*- endif *)) the execution instructions for JUBE. Find details in \texttt{DESCRIPTION.md}."""
-
-    bench_artout_tpl = r"""
-    % this file is auto-generated, do not change it manually!
-    See details in \texttt{DESCRIPTION.md} in the benchmark's repository."""
-
-    tpl_suite_header = r"""
-    ((* if suite -*))\multicolumn{2}{l}{((( name )))} & 
-    ((*- else -*))
-    & ((( name ))) &
-    ((*- endif *))
-    ((*- for tag in tags *)) \((( tag[0] ))){((( tag[1] )))} ((*- endfor *)) &
-    ((* if license -*)) ((( license ))) ((* endif *)) & 
-    ((* if url['long'] -*)) \href{((( url['long'] )))}{((( url['short'] )))}} ((* endif *)) ((* if citekey -*)) \cite{((( citekey )))} ((* endif *)) & 
-    ((* if notes -*)) ((( notes ))) ((* endif *))\\
-    """
-
-    # for i, benchmark in enumerate(benchmarks):
-    #     print(f"Rendering template for {item}:{benchmark}")
-    #     base = 'Base' in appinfos[benchmark]['featured_figures']
-    #     weak = 'Weak' in appinfos[benchmark]['featured_figures']
-    #     number = i+1
-    #     modules = all_modules[benchmark]
-    #     jubefiles = appinfos[benchmark]['jube_files']
-    #     jube_run_paths = appinfos[benchmark]['jube_run_paths']
-    #     # jubefiles_other = appinfos[benchmark]['jube_files_other']
-    #     rendered_template = environment.from_string(
-    #             textwrap.dedent(template).strip()).render(
-    #                     number=number, name=benchmark,
-    #                     base=base, weak=weak, modules=modules,
-    #                     jubefiles=jubefiles,
-    #                     jube_run_paths=jube_run_paths,
-    #                     compile_strings=apps_compile_strings,
-    #                     run_strings=apps_run_strings,
-    #                     eval_strings=apps_eval_strings)
-    #     if args.print:
-    #         print(rendered_template)
-    #     if args.write:
-    #         outfile = f"{folder}/{benchmark.replace(' ', '-')}.tex"
-    #         with open(outfile, mode="w", encoding="utf-8") as output:
-    #             print(f'Writing to {outfile}', file=sys.stderr)
-    #             output.write(rendered_template)
-
     benchmark_tex = ""
     sorted_benchmarks = sorted(benchmarks.items(), key=lambda b: b[0].startswith('benchmark'))  # sort "benchmarks key to the end (https://stackoverflow.com/a/57301615)
-    for benchkey, benchdata in dict(sorted_benchmarks).items():
-        if benchkey == "benchmarks":  # go to next loop
-            continue
-        # print(benchdata)
-        name = benchdata['name']
-        license = benchdata['license'] if 'license' in benchdata else ''
-        notes = benchdata['notes'] if 'notes' in benchdata else ''
-        ref = f'link_{benchkey}' if 'ref' in benchdata else ''
-        tags = parseTags(benchdata['tags']) if 'tags' in benchdata else ''
-        url = {
-            'long': benchdata['url'] if 'url' in benchdata else '',
-            'short': benchdata['url'].replace('https://', '').replace('http://', '') if 'url' in benchdata else ''
-        }
-        suite=True
-        print(environment.from_string(tpl_suite_header).render(name=name, license=license, notes=notes, citekey=ref, tags=tags, url=url, suite=suite))
+    for suitekey, suitedata in dict(sorted_benchmarks).items():
+        if suitekey != "benchmarks":  # go to next loop
+            benchmark_tex += genTableRow(suitekey, suitedata, suite=True, jinjaenv=environment)
+            benchmarks = suitedata['benchmarks']
+        else:
+            benchmarks = suitedata
+        for benchkey, benchdata in benchmarks.items():
+            benchmark_tex += genTableRow(benchkey, benchdata, suite=False, jinjaenv=environment)
+    if args.print:
+        print(benchmark_tex)
+    if args.write:
+        outfile = f"benchmark_table_data.tex"
+        with open(outfile, mode="w", encoding="utf-8") as output:
+            print(f'Writing to {outfile}', file=sys.stderr)
+            output.write(benchmark_tex)
+def genTableRow(benchkey, benchdata, suite, jinjaenv):
+    name = benchdata['name']
+    license = benchdata['license'] if 'license' in benchdata else ''
+    notes = benchdata['notes'] if 'notes' in benchdata else ''
+    ref = f'link_{benchkey}' if 'ref' in benchdata else ''
+    tags = parseTags(benchdata['tags']) if 'tags' in benchdata else ''
+    url = {
+        'long': benchdata['url'] if 'url' in benchdata else '',
+        'short': benchdata['url'].replace('https://', '').replace('http://', '') if 'url' in benchdata else ''
+    }
+    return jinjaenv.from_string(textwrap.dedent(tpl_suite_header).strip()).render(key=benchkey, name=name, license=license, notes=notes, citekey=ref, tags=tags, url=url, suite=suite)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='HPC Benchmarks Survey Table Generator', description='Generate a LaTeX table from YAML')
     parser.add_argument('-p', '--print', action='store_true', default=True, help='Print generated template(s)')
