b5ad47d1fb1a0c745bd01c9c0f8aab9ff716e363
Andreas Herten <a.herten@fz-juelich.de>
2024-07-23 13:26:32 +0200

    move over to tabularray

--- tex-gen/table-generator.py
+++ tex-gen/table-generator.py
@@ -14,6 +14,15 @@ tags_cats_to_tex_commands = {
     'communication': 'btComm',
     'application-domain': 'btAppDomain'
 }
+license_to_tex_commands = {
+    'BSD': r'\licBsd',
+    'BSD-3-Clause': r'\licBsdThree',
+    'MIT': r'\licMit',
+    'None': r'\licNone',
+    'Free': r'\licFree',
+    'GPL': r'\licGpl',
+    'GPL-3.0': r'\licGplThree'
+}
 sort_order_tags = {cat: (i, cat) for i, cat in enumerate(["programming-language", "programming-model"])}
 def customSortTags(tags):
     return sorted(tags, key=lambda cat: sort_order_tags.get(cat[0], (len(sort_order_tags), cat[0]))) #https://stackoverflow.com/a/64593268
@@ -24,16 +33,32 @@ def parseTags(rawTags):
         cat = tags_cats_to_tex_commands[rawcat] if rawcat in tags_cats_to_tex_commands else 'NA'
         _tags.append((cat, name))
     return _tags
-tpl_suite_header = r"""
-    ((* if multicol -*))\multicolumn{2}{l}{((( name )))} & 
+tpl_suite_tableheader = r"""
+\begin{longtblr}[
+    caption={Benchmark Overview}
+]{
+    width   =\textwidth,
+    colspec ={X[0.01]X[0.8,l]X[1.4]ccX[3]},
+    cells   = {font=\footnotesize},
+    row{1}  = {font=\footnotesize\bfseries}, rowhead = 1,
+    rowsep = 0.3pt
+}
+\SetCell[c=2]{l} \emph{(Suite-)}Name & & Tags & License & URL, Reference & Notes \\
+\midrule"""
+tpl_suite_tablefooter = r"""
+\end{longtblr}
+"""
+tpl_suite_table = r"""
+    ((* if multicol -*))((* if suitekey -*))\tikzmark{((( suitekey ))) start}((* endif-*))\SetCell[c=2]{l}((( name ))) & &
     ((*- else -*))
-    & ((( name ))) &
+    ((* if last_suite_bench -*))\tikzmark{((( suitekey ))) end}\connectorLine{((( suitekey ))) start}{((( suitekey ))) end} ((*- endif *))& ((( name ))) &
     ((*- endif *))
     ((* for tag in tags *)) \((( tag[0] ))){((( tag[1] )))} ((*- endfor *)) &
     ((* if license -*)) ((( license ))) ((* endif -*)) & 
-    ((* if url['long'] -*)) \href{((( url['long'] )))}{\url{((( url['short'] )))}} ((* endif -*)) ((* if citekey -*)) \cite{((( citekey )))} ((* endif -*)) & 
+    ((* if url['key'] -*)) \cite{((( url['key'] )))} \href{((( url['long'] )))}{\scalebox{0.8}{\faIcon{link}}}((* endif -*)) ((* if url and citekey -*)), ((* endif -*)) ((* if citekey -*)) \cite{((( citekey )))} ((* endif -*)) & 
     ((* if notes -*)) ((( notes ))) ((* endif -*))\\
 """
+
 def main(args):
 
     with open('benchmarks.yaml', 'r') as f:
@@ -53,36 +78,44 @@ def main(args):
         loader=BaseLoader
     )
 
-    benchmark_tex = ""
+    benchmark_tex = tpl_suite_tableheader
     sorted_benchmarks = sorted(benchmarks.items(), key=lambda b: b[0].startswith('benchmark'))  # sort "benchmarks key to the end (https://stackoverflow.com/a/57301615)
     for suitekey, suitedata in dict(sorted_benchmarks).items():
         multicol=True
+        last_suite_bench=False
+        insuite=True
         if suitekey != "benchmarks":  # go to next loop
-            benchmark_tex += genTableRow(suitekey, suitedata, multicol=multicol, jinjaenv=environment)
+            benchmark_tex += genTableRow(suitekey, suitedata, suitekey=suitekey, last_suite_bench=last_suite_bench, multicol=multicol, jinjaenv=environment)
             benchmarks = suitedata['benchmarks']
             multicol=False
         else:
             benchmarks = suitedata
+            insuite=False
+            suitekey=""
         for benchkey, benchdata in benchmarks.items():
-            benchmark_tex += genTableRow(benchkey, benchdata, multicol=multicol, jinjaenv=environment)
+            if insuite and (benchkey == list(benchmarks.keys())[-1]):
+                last_suite_bench=True
+            benchmark_tex += genTableRow(benchkey, benchdata, suitekey=suitekey, last_suite_bench=last_suite_bench, multicol=multicol, jinjaenv=environment)
+    benchmark_tex += tpl_suite_tablefooter
     if args.print:
         print(benchmark_tex)
     if args.write:
-        outfile = f"benchmark_table_data.tex"
+        outfile = f"benchmark_table.tex"
         with open(outfile, mode="w", encoding="utf-8") as output:
             print(f'Writing to {outfile}', file=sys.stderr)
             output.write(benchmark_tex)
-def genTableRow(benchkey, benchdata, suite, jinjaenv):
+def genTableRow(benchkey, benchdata, suitekey, last_suite_bench, multicol, jinjaenv):
     name = benchdata['name']
-    license = benchdata['license'] if 'license' in benchdata else ''
+    license = license_to_tex_commands.get(benchdata['license'], benchdata['license']) if 'license' in benchdata else ''
     notes = benchdata['notes'] if 'notes' in benchdata else ''
-    ref = f'link_{benchkey}' if 'ref' in benchdata else ''
+    ref = f'{benchkey}_ref' if 'ref' in benchdata else ''
     tags = parseTags(benchdata['tags']) if 'tags' in benchdata else ''
     url = {
         'long': benchdata['url'] if 'url' in benchdata else '',
-        'short': benchdata['url'].replace('https://', '').replace('http://', '') if 'url' in benchdata else ''
+        'short': benchdata['url'].replace('https://', '').replace('http://', '') if 'url' in benchdata else '',
+        'key': f'{benchkey}_link' if 'url' in benchdata else ''
     }
-    return jinjaenv.from_string(textwrap.dedent(tpl_suite_header)).render(key=benchkey, name=name, license=license, notes=notes, citekey=ref, tags=tags, url=url, suite=suite)
+    return jinjaenv.from_string(textwrap.dedent(tpl_suite_table)).render(key=benchkey, name=name, license=license, notes=notes, citekey=ref, tags=tags, url=url, multicol=multicol, suitekey=suitekey, last_suite_bench=last_suite_bench)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='HPC Benchmarks Survey Table Generator', description='Generate a LaTeX table from YAML')
     parser.add_argument('-p', '--print', action='store_true', default=True, help='Print generated template(s)')
