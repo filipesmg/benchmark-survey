62eb3113779486d337b2a63b434fb26dbc993fb7
Andreas Herten <a.herten@fz-juelich.de>
2024-08-14 23:12:36 +0200

    Add: LaTeX character escape, more licenses, generation of link bibtex entries

--- tex-gen/table-generator.py
+++ tex-gen/table-generator.py
@@ -2,6 +2,7 @@ import sys
 import argparse
 
 import json
+import re
 import textwrap
 import yaml
 from jinja2 import Environment, BaseLoader
@@ -21,11 +22,14 @@ tags_cats_to_tex_commands = {
 license_to_tex_commands = {
     'bsd': r'\licBsd',
     'bsd-3-clause': r'\licBsdThree',
+    'bsd-2-clause': r'\licBsdTwo',
     'mit': r'\licMit',
     'none': r'\licNone',
     'free': r'\licFree',
     'gpl': r'\licGpl',
-    'gpl-3.0': r'\licGplThree'
+    'gpl-3.0': r'\licGplThree',
+    'proprietary': r'\licProp',
+    'apache-2.0': r'\licApacheTwo',
 }
 sort_order_tags = {cat: (i, cat) for i, cat in enumerate(["programming-language", "programming-model"])}
 def customSortTags(tags):
@@ -37,6 +41,28 @@ def parseTags(rawTags):
         cat = tags_cats_to_tex_commands[rawcat] if rawcat in tags_cats_to_tex_commands else 'NA'
         _tags.append((cat, name))
     return _tags
+def tex_escape(text):
+    """
+        :param text: a plain text message
+        :return: the message escaped to appear correctly in LaTeX
+        credit: https://stackoverflow.com/a/25875504
+    """
+    conv = {
+        '&': r'\&',
+        '%': r'\%',
+        '$': r'\$',
+        '#': r'\#',
+        '_': r'\_',
+        '{': r'\{',
+        '}': r'\}',
+        '~': r'\textasciitilde{}',
+        '^': r'\^{}',
+        '\\': r'\textbackslash{}',
+        '<': r'\textless{}',
+        '>': r'\textgreater{}',
+    }
+    regex = re.compile('|'.join(re.escape(str(key)) for key in sorted(conv.keys(), key = lambda item: - len(item))))
+    return regex.sub(lambda match: conv[match.group()], text)
 tpl_suite_tableheader = r"""% do not change this file manually! it's auto-generated
 \begin{longtblr}[
     caption={Benchmark Overview. Benchmark suites are indicated by leftmost entries with individual subordinated benchmark entries, connected by a dotted line. Per benchmark, tags, license (\emph{Lic.}), URL, reference (\emph{Ref.}), and notes are given; if each component is common to all benchmarks in a suite, the components are listed for the suite itself.},
@@ -64,7 +90,12 @@ tpl_suite_table = r"""
     ((* if url['key'] -*))\href{((( url['long'] )))}{\scalebox{0.8}{\faIcon{link}}}~\cite{((( url['key'] )))}((* endif -*)) ((* if url and citekey -*)), ((* endif -*)) ((* if citekey -*)) \cite{((( citekey )))} ((* endif -*)) &
     ((* if notes -*)) ((( notes ))) ((* endif -*))\\
 """
-
+tpl_bib_link = r"""
+@electronic{((( key ))),
+ title     = {{((( name )))}},
+ url       = {((( url )))}
+}
+"""
 def main(args):
 
     with open('benchmarks.yaml', 'r') as f:
@@ -85,6 +116,7 @@ def main(args):
     )
 
     benchmark_tex = tpl_suite_tableheader
+    bib_urls_tex = ""
     sorted_benchmarks = sorted(benchmarks.items(), key=lambda b: b[0].startswith('benchmark'))  # sort "benchmarks key to the end (https://stackoverflow.com/a/57301615)
     for suitekey, suitedata in dict(sorted_benchmarks).items():
         multicol=True
@@ -92,6 +124,7 @@ def main(args):
         insuite=True
         if suitekey != "benchmarks":  # go to next loop
             benchmark_tex += genTableRow(suitekey, suitedata, suitekey=suitekey, last_suite_bench=last_suite_bench, multicol=multicol, jinjaenv=environment)
+            bib_urls_tex += genBibUrl(suitekey, suitedata, jinjaenv=environment)
             benchmarks = suitedata['benchmarks']
             multicol=False
         else:
@@ -102,18 +135,24 @@ def main(args):
             if insuite and (benchkey == list(benchmarks.keys())[-1]):
                 last_suite_bench=True
             benchmark_tex += genTableRow(benchkey, benchdata, suitekey=suitekey, last_suite_bench=last_suite_bench, multicol=multicol, jinjaenv=environment)
+            bib_urls_tex += genBibUrl(benchkey, benchdata, jinjaenv=environment)
     benchmark_tex += tpl_suite_tablefooter
     if args.print:
         print(benchmark_tex)
+        print(bib_urls_tex)
     if args.write:
         outfile = f"benchmark_table.tex"
         with open(outfile, mode="w", encoding="utf-8") as output:
             print(f'Writing to {outfile}', file=sys.stderr)
-            output.write(benchmark_tex)
+            output.write(benchmark_tex) 
+        outfile = f"benchmarklinks.bib"
+        with open(outfile, mode="w", encoding="utf-8") as output:
+            print(f'Writing to {outfile}', file=sys.stderr)
+            output.write(bib_urls_tex)
 def genTableRow(benchkey, benchdata, suitekey, last_suite_bench, multicol, jinjaenv):
-    name = benchdata['name']
+    name = tex_escape(benchdata['name'])
     license = license_to_tex_commands.get(benchdata['license'].lower(), benchdata['license']) if 'license' in benchdata else ''
-    notes = benchdata['notes'] if 'notes' in benchdata else ''
+    notes = tex_escape(benchdata['notes']) if 'notes' in benchdata else ''
     ref = f'{benchkey}_ref' if 'ref' in benchdata else ''
     tags = parseTags(benchdata['tags']) if 'tags' in benchdata else ''
     url = {
@@ -122,6 +161,14 @@ def genTableRow(benchkey, benchdata, suitekey, last_suite_bench, multicol, jinja
         'key': f'{benchkey}_link' if 'url' in benchdata else ''
     }
     return jinjaenv.from_string(textwrap.dedent(tpl_suite_table)).render(key=benchkey, name=name, license=license, notes=notes, citekey=ref, tags=tags, url=url, multicol=multicol, suitekey=suitekey, last_suite_bench=last_suite_bench)
+def genBibUrl(benchkey, benchdata, jinjaenv):
+    if 'url' in benchdata:
+        name = tex_escape(benchdata['name'])
+        url = benchdata['url'] 
+        key = f'{benchkey}_link'
+        return jinjaenv.from_string(textwrap.dedent(tpl_bib_link)).render(key=key, name=name, url=url)
+    else:
+        return ""
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='HPC Benchmarks Survey Table Generator', description='Generate a LaTeX table from YAML')
     parser.add_argument('-p', '--print', action='store_true', default=True, help='Print generated template(s)')
